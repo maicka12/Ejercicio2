a06d728d1cd4a22c7c94a02c5f6ae7dbc826dba7
maicka <maicka1997@gmail.com>
2022-07-05 03:02:12 +0200

    Agregar archivos de proyecto.

---
+++ exercici1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            int N = 5;
+            double A = 4.56;
+            char C = 'b';
+            int b = 97;
+
+            int B = Convert.ToInt32(A);
+
+            int suma = B + N;
+            int resta = B - N;
+            
+            Console.WriteLine(suma);
+            Console.WriteLine(resta);
+            Console.WriteLine(C);
+            Console.WriteLine(b);
+            Console.ReadKey();
+
+            
+
+
+        }
+
+    }
+}
